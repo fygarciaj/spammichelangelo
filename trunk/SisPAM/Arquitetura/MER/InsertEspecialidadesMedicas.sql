e4adf7935fef7687fc9f5f09941a73db5199cece
leoseagal <leoseagal@0b76ed62-0f76-11de-b8e2-c32c05206e41>
2009-05-11 21:31:35 +0000

    inclusao da biblioteca do ireport

---
+++ trunk/SisPAM/Arquitetura/MER/InsertEspecialidadesMedicas.sql
@@ -0,0 +1,119 @@
+INSERT INTO especialidademedica(emddes)
+    VALUES ('ACUPUNTURA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('ALERGIA E IMUNOLOGIA'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('ANESTESIOLOGIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('ANGIOLOGIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('CIRURGIA VASCULAR');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('CANCEROLOGIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('CARDIOLOGIA'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('CIRURGIA CARDIOVASCULAR'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('CIRURGIA DA MÃO'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('CIRURGIA DE CABEÇA E PESCOÇO'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('CIRURGIA GERAL'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('CIRURGIA DO APARELHO DIGESTIVO'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('CIRURGIA PEDIÁTRICA'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('CIRURGIA PLÁSTICA'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('CIRURGIA TORÁCICA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('CLÍNICA MÉDICA'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('COLOPROCTOLOGIA'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('DERMATOLOGIA'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('ENDOCRINOLOGIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('ENDOSCOPIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('GASTROENTEROLOGIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('GENÉTICA MÉDICA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('GERIATRIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('GINECOLOGIA E OBSTETRÍCIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('HEMATOLOGIA E HEMOTERAPIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('HOMEOPATIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('INFECTOLOGIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('MASTOLOGIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('MEDICINA DE FAMÍLIA E COMUNIDADE');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('MEDICINA DO TRABALHO');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('MEDICINA DO TRÁFEGO');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('MEDICINA DO VIAJANTE'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('MEDICINA ESPORTIVA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('MEDICINA FÍSICA E REABILITAÇÃO');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('MEDICINA INTENSIVA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('MEDICINA LEGAL');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('MEDICINA NUCLEAR'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('MEDICINA PREVENTIVA E SOCIAL'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('NEFROLOGIA'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('NEUROCIRURGIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('NEUROLOGIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('NUTROLOGIA'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('OFTALMOLOGIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('ORTOPEDIA'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('TRAUMATOLOGIA'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('OTORRINOLARINGOLOGIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('PATOLOGIA'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('PATOLOGIA CLÍNICA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('PEDIATRIA'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('PNEUMOLOGIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('PSIQUIATRIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('RADIOLOGIA'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('DIAGNÓSTICO POR IMAGEM'); 
+INSERT INTO especialidademedica(emddes)
+    VALUES ('RADIOTERAPIA REUMATOLOGIA');
+INSERT INTO especialidademedica(emddes)
+    VALUES ('UROLOGIA'); 
+
+
+
+
+
+
+
+
+
